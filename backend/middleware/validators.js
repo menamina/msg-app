a5521105edd23c48a6d10823a990da14d86ec71f
Mena <dayjacs03@gmail.com>
2026-02-08 16:44:09 -0600

    adding validators

---
+++ backend/middleware/validators.js
@@ -0,0 +1,34 @@
+import { body, validationResult } from "express-validator";
+
+const validator = [
+  body("name").notEmpty().withMessage("Name is required"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail.withMessage("Email must be valid"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Email must be at least 8 characters"),
+
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords must match");
+    } else {
+      return true;
+    }
+  }),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      next();
+    }
+  },
+];
