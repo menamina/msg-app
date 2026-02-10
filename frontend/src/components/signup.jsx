f5d51425bb1add811faab3c81547e63fffc00efa
Mena <dayjacs03@gmail.com>
2026-02-09 18:21:30 -0600

    working on signup

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -2,9 +2,31 @@ import { useEffect, useState } from "react";
 import { Link, useOutletContext, useNavigate } from "react-router-dom";
 
 function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  const navigate = useNavigate();
+
   async function signupAPI() {
-    const form = new FormData();
-    form.append("name");
+    try {
+      const res = await fetch("http://localhost:5000/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+          confirmPass: confirmPass,
+        }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        return console.log(data.error);
+      } else navigate("/login");
+    } catch (error) {
+      console.log("something went wrong trying to signup", error.message);
+    }
   }
 
   return (
@@ -12,22 +34,38 @@ function Signup() {
       <form onSubmi={signupAPI}>
         <div>
           <label htmlFor="">Name:</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="">Email:</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="">Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="">Confirm Password:</label>
-          <input type="password" />
+          <input
+            type="password"
+            value={confirmPass}
+            onChange={(e) => setConfirmPass(e.target.value)}
+          />
         </div>
       </form>
     </div>
