126910446c5248d763749491f879415f314d328b
Mena <dayjacs03@gmail.com>
2026-02-19 18:44:21 -0600

    working on friend req nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import "./css/index.css";
 
 function App() {
   const [user, setUser] = useState(null);
--- frontend/src/components/friendReq.jsx
+++ frontend/src/components/friendReq.jsx
@@ -1,6 +1,40 @@
 import { useState, useEffect } from "react";
 import { Link, useOutletContext } from "react-router-dom";
 
-function FriendReq() {}
+function FriendReq() {
+  const [friendRequests, setFriendRequests] = useState([]);
+  const [noFriendReqsMsg, setNoFriendReqsMsg] = useState(null);
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    async function getFriendReqs() {
+      try {
+        const res = await fetch("http://localhost:5555/getFriendReqs", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          setNoFriendReqsMsg(data.message);
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getFriendReqs();
+  }, []);
+
+  return (
+    <div>
+        { noFriendReqsMsg && <div>{noFriendReqsMsg}</div> }
+        {friendRequests && 
+
+        }
+    </div>
+  )
+
+}
 
 export default FriendReq;
