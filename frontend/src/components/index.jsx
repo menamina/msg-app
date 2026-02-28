7542277290b4a5fa08bfe0ce53286a4c974e5d3d
Mena <dayjacs03@gmail.com>
2026-02-27 19:53:25 -0600

    css

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -4,13 +4,13 @@ import "../css/index.css";
 function Index() {
   return (
     <div className="index div">
-      <div>
+      <div className="welcome">
         <div>Welcome to Cyberspace,</div>
         <div>the messaging app</div>
         <div>where you can send messages from anywhere, to anyone</div>
       </div>
 
-      <div>
+      <div className="right">
         <div>
           <Link to="/signup">Sign up</Link> today
         </div>
