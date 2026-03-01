68f56994e34ffc611c83c1efc4c1700f7b7113fb
Mena <dayjacs03@gmail.com>
2026-03-01 15:42:05 -0600

    testing

---
+++ backend/testing/backendTesting/apiRouteTesting.supertest.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest");
+const app = require("./app");
+
+test("that you can sign up", async () => {});
+test("that you can login", async () => {});
+test("that you can logout", async () => {});
+test("that you can find a session", async () => {});
